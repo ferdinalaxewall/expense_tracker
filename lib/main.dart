97d2a478542a8daea665f25ce7670234c4063df0
Ferdinalaxewall <86649049+ferdinalaxewall@users.noreply.github.com>
2024-06-27 02:24:01 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:expense_tracker/widgets/expenses.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: GoogleFonts.raleway(
+                textStyle: const TextStyle(
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              bodySmall: GoogleFonts.raleway(
+                textStyle: const TextStyle(
+                  fontWeight: FontWeight.w400
+                )
+              ),
+              labelLarge: GoogleFonts.raleway(
+                textStyle: const TextStyle(
+                  fontWeight: FontWeight.w600
+                )
+              ),
+              bodyMedium: GoogleFonts.lato(
+                textStyle: const TextStyle(
+                  fontWeight: FontWeight.w400
+                )
+              ),
+            ),
+      ),
+      home: const Expenses(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,112 @@
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() => _ExpensesState();
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: ExpenseCategory.leisure,
+    ),
+    Expense(
+      title: 'Fried Chicken',
+      amount: 5.00,
+      date: DateTime.now(),
+      category: ExpenseCategory.food,
+    ),
+    Expense(
+      title: 'Japan Tour',
+      amount: 200.00,
+      date: DateTime.now(),
+      category: ExpenseCategory.travel,
+    ),
+    Expense(
+      title: 'Frontend Developer',
+      amount: 450.00,
+      date: DateTime.now(),
+      category: ExpenseCategory.work,
+    ),
+  ];
+
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
+      builder: (ctx) => NewExpense(_addExpense),
+    );
+  }
+
+  void _addExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.add(expense);
+    });
+  }
+
+  void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
+
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text('Expense deleted!'),
+        action: SnackBarAction(
+            label: 'Undo',
+            onPressed: () {
+              setState(() {
+                _registeredExpenses.insert(expenseIndex, expense);
+              });
+            }),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text('No expanses found. Try to adding some!'),
+    );
+
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Expense Tracker',
+        ),
+        centerTitle: false,
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          const Text('The Chart'),
+          Expanded(child: mainContent),
+        ],
+      ),
+    );
+  }
+}
